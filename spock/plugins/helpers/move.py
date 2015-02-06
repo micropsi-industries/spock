f69bb5a77e25de39515738d8c2e004ce2c1bc170
Dominik Welland <hallo@doik.de>
2015-02-06 15:25:04 +0100

    make movementplugin use clientinfo.position

--- spock/plugins/helpers/move.py
+++ spock/plugins/helpers/move.py
@@ -10,6 +10,7 @@ from spock.mcp import mcdata, mcpacket
 class MovementPlugin:
 	def __init__(self, ploader, settings):
 		self.net = ploader.requires('Net')
+		self.clientinfo = ploader.requires('ClientInfo')
 		ploader.reg_event_handler(
 			(mcdata.PLAY_STATE, mcdata.SERVER_TO_CLIENT, 0x08),
 			self.handle_position_look
@@ -19,44 +20,37 @@ class MovementPlugin:
 			self.handle_respawn
 		)
 		ploader.reg_event_handler('client_tick', self.client_tick)
-		#TODO: use clientInfo plugin instead
-		self.position = {
-			'x': 0,
-			'y': 0,
-			'z': 0,
-			'yaw': 0,
-			'pitch': 0,
-			'on_ground': False,
-		}
 
 	def handle_position_look(self, name, packet):
-		self.position['x'] = packet.data['x']
-		self.position['y'] = packet.data['y']
-		self.position['z'] = packet.data['z']
-		self.position['pitch'] = packet.data['pitch']
-		self.position['pitch'] = packet.data['yaw']
-
+		self.clientinfo.position['x'] = packet.data['x']
+		self.clientinfo.position['y'] = packet.data['y']
+		self.clientinfo.position['z'] = packet.data['z']
+		self.clientinfo.position['pitch'] = packet.data['pitch']
+		self.clientinfo.position['yaw'] = packet.data['yaw']
 		self.net.push(mcpacket.Packet(
 			ident = (mcdata.PLAY_STATE, mcdata.CLIENT_TO_SERVER, 0x06),
 			#TODO: check flags to see if Absolute vs Relative
 			data = {
-				'x': self.position['x'],
-				'y': self.position['y'],
-				'z': self.position['z'],
-				'pitch': self.position['pitch'],
-				'yaw': self.position['yaw'],
+				'x': self.clientinfo.position['x'],
+				'y': self.clientinfo.position['y'],
+				'z': self.clientinfo.position['z'],
+				'pitch': self.clientinfo.position['pitch'],
+				'yaw': self.clientinfo.position['yaw'],
 				'on_ground': True
 			}
 		))
 
 	def client_tick(self, name, data):
 		self.net.push(mcpacket.Packet(
-			ident = (mcdata.PLAY_STATE, mcdata.CLIENT_TO_SERVER, 0x04),
+			ident = (mcdata.PLAY_STATE, mcdata.CLIENT_TO_SERVER, 0x06),
 			#TODO: check flags to see if Absolute vs Relative
 			data = {
-				'x': self.position['x'],
-				'y': self.position['y'],
-				'z': self.position['z'],
+				'x': self.clientinfo.position['x'],
+				'y': self.clientinfo.position['y'],
+				'z': self.clientinfo.position['z'],
+				'stance': self.clientinfo.position['stance'],
+				'pitch': self.clientinfo.position['pitch'],
+				'yaw': self.clientinfo.position['yaw'],
 				'on_ground': True
 			}
 		))
@@ -66,11 +60,11 @@ class MovementPlugin:
 			ident = (mcdata.PLAY_STATE, mcdata.CLIENT_TO_SERVER, 0x06),
 			#TODO: check flags to see if Absolute vs Relative
 			data = {
-				'x': self.position['x'],
-				'y': self.position['y'],
-				'z': self.position['z'],
-				'pitch': self.position['pitch'],
-				'yaw': self.position['yaw'],
+				'x': self.clientinfo.position['x'],
+				'y': self.clientinfo.position['y'],
+				'z': self.clientinfo.position['z'],
+				'pitch': self.clientinfo.position['pitch'],
+				'yaw': self.clientinfo.position['yaw'],
 				'on_ground': False
 			}
 		))
